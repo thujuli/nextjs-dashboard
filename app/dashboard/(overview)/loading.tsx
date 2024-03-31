299b39a4dc862c393cdf765845c455635ff0b116
thujuli <thujuli037@gmail.com>
2024-03-31 15:42:53 +0700

    Complete nextjs-dashboard tutorial

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DashboardSkeleton from '../../ui/skeletons';
+
+const DashboardLoading = () => {
+  return <DashboardSkeleton />;
+};
+
+export default DashboardLoading;
